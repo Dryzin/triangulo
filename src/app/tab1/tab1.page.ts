43682145237c532c9382c71eaefb8b7443785e98
Dryzin <endrioemanuel11@gmail.com>
2022-08-26 11:27:43 -0300

    Atividade triangulo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss', '../explore-container/explore-container.component.scss']
 })
 export class Tab1Page {
 
+  ld1: number
+  ld2: number
+  ld3: number
+  inf: string
+  imagem: any = "assets/icon/5.jpg"
+
   constructor() {}
 
+  conta(){
+
+      if (this.ld1 + this.ld2 > this.ld3 && this.ld1 + this.ld3 > this.ld2 && this.ld2 + this.ld3 > this.ld1) {
+        
+        if (this.ld1 == this.ld2 && this.ld2 == this.ld3) {
+          this.imagem = "assets/icon/1.webp"
+          this.inf = "Equilátero"
+        }
+
+        else if (this.ld1 != this.ld2 && this.ld2 != this.ld3 && this.ld1 != this.ld3) {
+          this.imagem = "assets/icon/3.webp"
+          this.inf = "Escaleno"
+        }
+
+        else {
+          this.imagem = "assets/icon/2.webp"
+          this.inf = "Isósceles"
+        }
+      }
+      
+
+      else{
+        this.inf = "Triangulo inexistente"
+        this.imagem = "assets/icon/favicon.png"
+      }
+
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss', '../explore-container/explore-container.component.scss']
 })
 export class Tab2Page {
+  lado_B : number
+  lado_A : number
+  valor : string
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'A Area é:',
+      subHeader: this.valor,
+      message: '',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  calcular(){
+
+    var resu = (this.lado_B*this.lado_A)/2
+
+    this.valor = resu.toString()
+
+    this.presentAlert()
+  }
 }
